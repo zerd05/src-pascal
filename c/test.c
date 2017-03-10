fd306a696cd0642e0488dd6c067fc24eb3d323d7
zerd05 <zerdltc@gmail.com>
2017-03-10 14:27:37 +0300

    Test
    Test

---
+++ c/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+    for(int i = 2; i>0;i++)
+    {
+    printf("hello,world %i\n",i);
+        i=i*i;
+    }
+        return 0;
+}
---
+++ xz.pas
@@ -0,0 +1,7 @@
+program aaa;
+var a,b,c:integer;
+begin
+readln(a,b);
+c:=a*b;
+writeln(c);
+end.
