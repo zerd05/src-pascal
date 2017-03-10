a450600583f3d9bd69d74e865259664e2be2252f
zerd05 <zerdltc@gmail.com>
2017-03-10 20:56:22 +0300

    ALLO

--- DZ_10.03.17/dz1.pas
+++ DZ_10.03.17/dz1.pas
@@ -1,15 +1,18 @@
+{*Даны 3 целых числа a,b,c проверить истинность высказывания число b находится между числами a и c
+    Грядущий Денис П-4-16 валерчик(Тынков) и Колян
+    *}
 program zzz;
 var a,b,c:integer;
 i:boolean;
 begin
 readln(a,b,c);
 if (a>b) and (b>c) then
-  begin
- i:=true;
- writeln(i)
- end
+begin
+i:=true;
+writeln(i)
+end
 else if (a<b) and (b<c) then
- begin
+begin
 i:=true;
 writeln(i)
 end
