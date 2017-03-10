43db283898e533b7015bca4abe474b82f524a602
zerd05 <zerdltc@gmail.com>
2017-03-10 19:57:09 +0300

    Dz
    Add dz2

---
+++ DZ_10.03.17/dz2.pas
@@ -0,0 +1,12 @@
+program zzz;
+var num1,num2:integer;
+begin
+writeln('');
+readln(num1,num2);
+if num1>num2 then
+writeln('(2)второе число меньше')
+else if num1<num2 then
+writeln('(1)первое число меньше')
+else if num1 = num2 then
+writeln('числа равны');
+end.
